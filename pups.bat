0afe7ce166ac27338a3025660a4fd0b2fc1ecfb5
SamuelJamesDev <91440489+SamuelJamesDev@users.noreply.github.com>
2021-09-26 17:23:40 -0700

    Create pups.bat

---
+++ pups.bat
@@ -0,0 +1,49 @@
+@echo off
+color 0B
+echo 
+echo     ========================
+echo     *                      *
+echo     * ==================== *
+echo     * =                  = * 
+echo     * =                  = *  [********************************]
+echo     * =  0            0  = *  [--------------------------------]
+echo     * =     _ __ _ _     = *  [I made some ASCII puppies       ]
+echo     * =     -_    _-     = * /[--------------------------------]
+echo     * =       ----       = * \[********************************]
+echo     * =                  = * 
+echo _   * =                  = *   _
+echo :   * ==================== *   :
+echo ::  *                      *  ::
+echo ::  *    __       ____  __ *  ::
+echo  :::*  ------      --   -- *:::
+echo     *    --        ______  *
+echo     *               ====   *
+echo     *                      *
+echo     -___  ____________  ___-
+echo         ==            ==        
+echo         ==            ==        
+echo         ==            ==        
+echo        _==            ==_
+ping 127.0.0.1 -n 3 > nul
+
+echo v..v       /
+echo /_/\_____/
+echo    /\   /\
+echo   /  \ /  \
+ping 127.0.0.1 -n 3 > nul
+
+echo
+echo   _---_
+echo  / 0 0 \
+echo  V\ Y /V
+echo     -
+ping 127.0.0.1 -n 3 > nul
+
+echo
+echo  ___
+echo v. .v
+echo \ T /
+echo  \U/ //
+echo /   \ 
+
+pause
